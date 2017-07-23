22d86f9988584f6d917bd20ecb3fef7517c70c7e
Elías Grande Cásedas <elias.grande.casedas@gmail.com>
2017-07-23 17:59:14 +0200

    Add german (de) locale & release v3.0.2

--- src/ogame-trade-calculator.user.js
+++ src/ogame-trade-calculator.user.js
@@ -5,11 +5,11 @@
 // @downloadURL  https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/releases/latest.user.js
 // @updateURL    https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/releases/latest.meta.js
 // @icon         https://github.com/EliasGrande/OGameTradeCalculator/raw/master/dist/img/icon.png
-// @version      3.0.1
+// @version      3.0.2
 // @include      *://*.ogame.*/game/index.php?*page=*
 // @include      *://*.ogame.gameforge.*/game/index.php?*page=*
 // ==/UserScript==
-/*! OGame Trade Calculator (C) 2016 Elías Grande Cásedas | MIT | opensource.org/licenses/MIT */
+/*! OGame Trade Calculator (C) 2017 Elías Grande Cásedas | MIT | opensource.org/licenses/MIT */
 (function(){
 ////////////
 
@@ -649,6 +649,78 @@ I18N.set(/fr/,
 	RES_DEF : "Restorer les options par defaut",
 	// Config » Contact
 	CONTACT : "Contact"
+});
+
+/*! [i18n=de] by comfreak89 https://github.com/EliasGrande/OGameTradeCalculator/issues/2 */
+I18N.set(/de/,
+{
+	// Number separators
+	THO_SEP : ".",
+	DEC_SEP : ",",
+	// Menu button
+	MENU    : "Handel",
+	// Window title
+	TITLE   : "Handelsrechner",
+	CONFIG  : "Einstellungen",
+	// Actions
+	ACTION  : "Aktion",
+	BUY     : "Ich kaufe",
+	SELL    : "Ich verkaufe",
+	// Ratio
+	RATIO   : "Kurs",
+	ILLEGAL : "illegal",
+	MAX     : "Maximum",
+	REG     : "Regulär",
+	MIN     : "Minimum",
+	// Output
+	IN_EXCH : "Im Austausch für",
+	RESULT  : "Ergebnis",
+	SEND    : "Ich sende",
+	RECEIVE : "Ich erhalte",
+	RES     : "Ressourcen",
+	// Ships
+	LC_SHIP : "GT", // Abb. of "Large Cargo Ship"
+	SC_SHIP : "KT", // Abb. of "Small Cargo Ship"
+	OR      : "oder", // e.g. 100 (LC) or 500 (SC)
+	// Message
+	MESSAGE : "Nachricht",
+	// Place of delivery
+	WHERE   : "Lieferadresse",
+	PLANET  : "Planet",
+	MOON    : "Mond",
+	CUR_PLA : "Aktueller Planet",
+	SEL_CUR : "Wähle den aktuellen Planeten oder Mond",
+	// Config » Ratio list
+	RAT_LST : "Kursliste",
+	NAME    : "Name",
+	LEGAL   : "Legal",
+	YES     : "Ja",
+	NO      : "Nein",
+	DEFAULT : "Standard",
+	NEW     : "Neu",
+	// Config » Default values
+	DEF_VAL : "Standardwerte",
+	// Config » Abbs & keys
+	ABB_KEY : "Abkürzungen und Tasten für Autovervollständigung",
+	USE_ABB : "Benutze Abkürzungen wenn möglich",
+	UNABB : "Felder nicht abkürzen, wenn die Maus über ihnen ist",
+	ABB_MIL : "Abkürzung für Millionen",
+	ABB_THO : "Abkürzung für Tausend",
+	KEY_MIL : "Taste für Millionen (6 Nullen)",
+	KEY_THO : "Taste für Tausend (3 Nullen)",
+	// Config » Message tpl
+	MES_TPL : "Nachrichtenvorlage",
+	RES_DTP : "Standardvorlage wiederherstellen",
+	// Config » Import / Export
+	IE_CONF : "Import / Export Konfiguration",
+	IMPORT  : "Import",
+	EXPORT  : "Export",
+	// Config » Buttons
+	ACCEPT  : "Akzeptieren",
+	CANCEL  : "Abbrechen",
+	RES_DEF : "Standardeinstellungen wiederherstellen",
+	// Config » Contact
+	CONTACT : "Kontaktinformation"
 });
 /*! [/i18n] */
 
